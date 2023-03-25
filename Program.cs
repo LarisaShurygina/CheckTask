7288e0d74bb8356c4d258e72234bff59bd33abda
Larisa Shurygina <larisashurygina@gmail.com>
2023-03-26 00:06:08 +0300

    Массив 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] InitArray = new string[5] { "12345", "hello", "=)", "yes", "home"};
+string[] FinalArray = new string[InitArray.Length];
+
+void FormArrayOfStrings(string[] InitArray, string[] FinalArray)
+
+{
+    int count = 0;
+    for (int i = 0; i < InitArray.Length; i++)
+    {
+        if (InitArray[i].Length <= 3)
+        {
+            FinalArray[count] = InitArray[i];
+            count++;
+        }
+    }
+}
